6885115298405fbd8c5f6cf4c49598ee8ff3f0c8
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-03 15:51:01 -0400

    pushing

--- Video 108/use-effect-hook/src/App.jsx
+++ Video 108/use-effect-hook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,6 +10,24 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // Snippet for useEffect
+  // useEffect(() => {
+
+  // }, [])
+
+
+
+  useEffect(() => {
+    alert("Welcome to my page!")
+  }, [])
+
+  useEffect(() => {
+    alert("Count was changed!")
+  }, [count])
+  
+
+
 
   return (
     <>
