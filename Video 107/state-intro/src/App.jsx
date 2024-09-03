49080c07bdf31fa88972a86e7027422d896c514b
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-03 15:20:23 -0400

    c

--- Video 107/state-intro/src/App.jsx
+++ Video 107/state-intro/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 
+
+// useState - Hook
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
-      
+      <div>The count is {count}</div>
+      <button onClick={()=>{setCount(count + 1)}}>Update count</button>
     </>
   )
 }
