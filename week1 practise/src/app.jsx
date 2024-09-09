45b4f3a66c9bf280eba6d7f16ea227796073eb30
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-09 14:25:50 -0400

    updated

---
+++ week1 practise/src/app.jsx
@@ -0,0 +1,9 @@
+const element = (
+    <div title="outer div">
+        <h1>
+            Hello World!
+        </h1>
+    </div>
+);
+  
+ReactDOM.render(element, document.getElementById("contents"));
