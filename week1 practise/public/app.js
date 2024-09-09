61e0702778ecaeed44523e2c01ccdd557fc27da7
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-09 14:29:28 -0400

    updating

---
+++ week1 practise/public/app.js
@@ -0,0 +1,4 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+ReactDOM.render(element, document.getElementById("contents"));
