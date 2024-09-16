590e5ddec70e360e726826b0d4a656ffb19a329f
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-16 13:52:29 -0400

    commit

--- Week1/public/app.js
+++ Week1/public/app.js
@@ -63,6 +63,62 @@
 // const element = <IssueList />;
 // ReactDOM.render(element, document.getElementById('contents'));
 
+// class IssueRow extends React.Component {
+//     render() {
+//         const style = this.props.rowStyle;
+//         return (
+//             <tr>
+//                 <td style = {style}>{this.props.issue_id}</td>
+//                 <td style = {style}>{this.props.issue_title}</td>
+//             </tr>
+//         )
+//     }
+// }
+
+// class IssueFilter extends React.Component {
+//     render() {
+//         const rowStyle = { border: "1px solid silver", padding: 4 };
+//         return (
+//             <table style={{ borderCollapse: "collapse" }}>
+//                 <thead>
+//                     <tr>
+//                         <th style={rowStyle}>ID</th>
+//                         <th style={rowStyle}>Title</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <IssueRow rowStyle={rowStyle} issue_id={1} issue_title="Error in console when clicking Add"/>
+//                     <IssueRow rowStyle={rowStyle} issue_id={2} issue_title="Missing bottom border on panel"/>
+//                 </tbody>
+//             </table>
+//         )
+//     }
+// }
+
+// class IssueTable extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for IssueTable</div>
+//         )
+//     }
+// }
+
+// class IssueList extends React.Component {
+//     render() {
+//         return (
+//             <React.Fragment>
+//                 <h1>Issue Tracker</h1>
+//                 <IssueFilter />
+//                 {/* <hr />
+//                 <IssueTable />
+//                 <hr />
+//                 <IssueRow />
+//                 <hr /> */}
+//             </React.Fragment>
+//         );
+//     }
+// }
+
 const issues = [{
   id: 1,
   status: 'New',
@@ -87,6 +143,22 @@ class IssueRow extends React.Component {
   }
 }
 class IssueTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      issues: []
+    };
+  }
+  componentDidMount() {
+    this.loadData();
+  }
+  loadData() {
+    setTimeout(() => {
+      this.setState({
+        issues: initialIssues
+      });
+    }, 500);
+  }
   render() {
     const issueRows = this.props.issues.map(issue => /*#__PURE__*/React.createElement(IssueRow, {
       key: issue.id,
--- Week1/src/app.jsx
+++ Week1/src/app.jsx
@@ -1,3 +1,126 @@
+// const element = (
+//     <div title="outer div">
+//         <h1>Hello World! Welcome to Abhishek Chachad's website. Abhishek!!</h1>
+//     </div>
+// )
+// ReactDOM.render(element, document.getElementById('contents'));
+
+// class HelloWorld extends React.Component{
+//     render(){
+//         const continents = ['Africa','Asia', 'Australia', 'Europe'];
+//         const helloContinents = Array.from(continents, c=> Hello ${c}!);
+//         const message = helloContinents.join(' '); 
+//         return(
+//             <div title = "Outer div">
+//                 <h1>{message}</h1>
+//             </div>
+//         );
+//     }
+// }
+// const element = <HelloWorld />;
+// ReactDOM.render(element, document.getElementById('contents'));
+
+// class IssueRow extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for IssueRow</div>
+//         )
+//     }
+// }
+
+// class IssueFilter extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for IssueFilter</div>
+//         )
+//     }
+// }
+
+// class IssueTable extends React.Component {
+// render(){
+//     return(
+//         <div>This is a placeholder for IssueTable</div>
+//     )
+// }
+// }
+
+// class IssueList extends React.Component {
+//     render(){
+//         return(
+//             <React.Fragment>
+//                 <h1>Issue Tracker</h1>
+//                 <IssueFilter />
+//                 <hr />
+//                 <IssueTable />
+//                 <hr />
+//                 <IssueRow />
+//                 <hr />
+//             </React.Fragment>
+//         );
+//     }
+//     }
+
+// const element = <IssueList />;
+// ReactDOM.render(element, document.getElementById('contents'));
+
+
+
+// class IssueRow extends React.Component {
+//     render() {
+//         const style = this.props.rowStyle;
+//         return (
+//             <tr>
+//                 <td style = {style}>{this.props.issue_id}</td>
+//                 <td style = {style}>{this.props.issue_title}</td>
+//             </tr>
+//         )
+//     }
+// }
+
+// class IssueFilter extends React.Component {
+//     render() {
+//         const rowStyle = { border: "1px solid silver", padding: 4 };
+//         return (
+//             <table style={{ borderCollapse: "collapse" }}>
+//                 <thead>
+//                     <tr>
+//                         <th style={rowStyle}>ID</th>
+//                         <th style={rowStyle}>Title</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <IssueRow rowStyle={rowStyle} issue_id={1} issue_title="Error in console when clicking Add"/>
+//                     <IssueRow rowStyle={rowStyle} issue_id={2} issue_title="Missing bottom border on panel"/>
+//                 </tbody>
+//             </table>
+//         )
+//     }
+// }
+
+// class IssueTable extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for IssueTable</div>
+//         )
+//     }
+// }
+
+// class IssueList extends React.Component {
+//     render() {
+//         return (
+//             <React.Fragment>
+//                 <h1>Issue Tracker</h1>
+//                 <IssueFilter />
+//                 {/* <hr />
+//                 <IssueTable />
+//                 <hr />
+//                 <IssueRow />
+//                 <hr /> */}
+//             </React.Fragment>
+//         );
+//     }
+// }
+
 const issues = [
     {
         id: 1, 
@@ -13,7 +136,7 @@ const issues = [
         status: 'Assigned', 
         owner: 'Eddie', 
         effort: 14,
-        created: new Date('2018-08-17'), 
+        created: new Date('2018-08-16'), 
         due: new Date('2018-08-30'),
         title: 'Missing bottom border on panel',
     }
@@ -37,6 +160,18 @@ class IssueRow extends React.Component {
 }
 
 class IssueTable extends React.Component {
+    constructor(){
+        super();
+        this.state = { issues: [] };
+    }
+    componentDidMount(){
+        this.loadData();
+    }
+    loadData(){
+        setTimeout(() => {
+            this.setState({issues: initialIssues});
+        }, 500);
+    }
     render() {
         const issueRows = this.props.issues.map(issue => (
             <IssueRow key={issue.id} issue={issue} />
@@ -72,6 +207,8 @@ class IssueList extends React.Component {
         );
     }
 }
+
+
 
 const element = <IssueList />;
 ReactDOM.render(element, document.getElementById('contents'));
